5d80ab4a6cdbd63b39ba2feb55ea9725ada4690b
gchghgvhjhjn <aevantho1@gmail.com>
2023-08-21 08:36:38 +0700

    update

--- js/scripts.js
+++ js/scripts.js
@@ -89,73 +89,91 @@ $(document).ready(function () {
 let recognition;
 let isRecording = false;
 let isInitialOk = false;
-let recordedText = ''; // Thêm lại biến recordedText
+let recordedText = ""; // Thêm lại biến recordedText
 
 // Khởi tạo đối tượng SpeechRecognition để nhận diện giọng nói
-if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
-    recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-    recognition.continuous = true;
+if ("SpeechRecognition" in window || "webkitSpeechRecognition" in window) {
+  recognition = new (window.SpeechRecognition ||
+    window.webkitSpeechRecognition)();
+  recognition.continuous = true;
 
-    // Xử lý sự kiện kết quả từ việc nhận diện giọng nói
-    recognition.onresult = event => {
-        const transcript = event.results[event.results.length - 1][0].transcript;
-        console.log("Đã nhận dạng:", transcript);
+  // Xử lý sự kiện kết quả từ việc nhận diện giọng nói
+  recognition.onresult = (event) => {
+    const transcript = event.results[event.results.length - 1][0].transcript;
+    console.log("Đã nhận dạng:", transcript);
 
-        // Xử lý khi người dùng nói 'ok' lần đầu để bắt đầu ghi âm
-        if (!isRecording && transcript.toLowerCase().includes('ok')) {
-            if (!isInitialOk) {
-                alert("Kết nối thành công");
-                isInitialOk = true;
-            }
-            console.log("Bắt đầu ghi âm");
-            isRecording = true;
-            recordedText = ''; // Reset giá trị recordedText
-        }
-        // Xử lý khi đang ghi âm và người dùng nói các từ để chuyển đến liên kết tương ứng
-        else if (isRecording) {
-            recordedText += transcript;
+    // Xử lý khi người dùng nói 'ok' lần đầu để bắt đầu ghi âm
+    if (!isRecording && transcript.toLowerCase().includes("hello")) {
+      if (!isInitialOk) {
+        alert("Kết nối thành công");
+        isInitialOk = true;
+      }
+      console.log("Bắt đầu ghi âm");
+      isRecording = true;
+      recordedText = ""; // Reset giá trị recordedText
+    } // Xử lý khi người dùng nói 'kết thúc' để dừng việc ghi âm
+    else if (isRecording && transcript.toLowerCase().includes("kết thúc")) {
+      console.log("Kết thúc ghi âm");
+      alert("Đã kết thúc ghi âm");
+      isRecording = false;
+      recordedText = ""; // Reset giá trị recordedText
+      recognition.stop(); // Dừng việc nhận diện giọng nói
+    }
+    // Xử lý khi đang ghi âm và người dùng nói các từ để chuyển đến liên kết tương ứng
+    else if (isRecording) {
+      recordedText += transcript;
 
-            if (transcript.toLowerCase().includes('1')) {
-                window.location.href = '#about';
-                animateLink('#about');
-            } else if (transcript.toLowerCase().includes('2')) {
-                window.location.href = '#experience';
-                animateLink('#experience');
-            } else if (transcript.toLowerCase().includes('3')) {
-                window.location.href = '#education';
-                animateLink('#education');
-            } else if (transcript.toLowerCase().includes('4')) {
-                window.location.href = '#skills';
-                animateLink('#skills');
-            } else if (transcript.toLowerCase().includes('5')) {
-                window.location.href = '#interests';
-                animateLink('#interests');
-            } else if (transcript.toLowerCase().includes('6')) {
-                window.location.href = '#awards';
-                animateLink('#awards');
-            }
-            // Hàm để áp dụng animation và sau đó xóa lớp "animated" sau khi hoàn thành
-            function animateLink(link) {
-                const linkElement = document.querySelector(link);
-                linkElement.classList.add('animated');
-                linkElement.addEventListener('animationend', () => {
-                    linkElement.classList.remove('animated');
-                });
-            }
-        }
-    };
+      if (transcript.toLowerCase().includes("1")) {
+        window.location.href = "#about";
+        animateLink("#about");
+      } else if (transcript.toLowerCase().includes("2")) {
+        window.location.href = "#experience";
+        animateLink("#experience");
+      } else if (transcript.toLowerCase().includes("3")) {
+        window.location.href = "#education";
+        animateLink("#education");
+      } else if (transcript.toLowerCase().includes("4")) {
+        window.location.href = "#skills";
+        animateLink("#skills");
+      } else if (transcript.toLowerCase().includes("5")) {
+        window.location.href = "#interests";
+        animateLink("#interests");
+      } else if (transcript.toLowerCase().includes("6")) {
+        window.location.href = "#awards";
+        animateLink("#awards");
+      } else if (transcript.toLowerCase().includes("7")) {
+        window.location.href =
+          "https://m.facebook.com/profile.php/?id=100023433405773";
+      } else if (transcript.toLowerCase().includes("8")) {
+        window.location.href =
+          "https://m.youtube.com/channel/UCG2AfXQ9zIggntyjNa9_z5w";
+      } else if (transcript.toLowerCase().includes("9")) {
+        window.location.href = "https://www.tiktok.com/@trongmin12";
+      } else if (transcript.toLowerCase().includes("0")) {
+        window.location.href = "https://github.com/trongminne";
+      }
+      // Hàm để áp dụng animation và sau đó xóa lớp "animated" sau khi hoàn thành
+      function animateLink(link) {
+        const linkElement = document.querySelector(link);
+        linkElement.classList.add("animated");
+        linkElement.addEventListener("animationend", () => {
+          linkElement.classList.remove("animated");
+        });
+      }
+    }
+  };
 
-    // Xử lý sự kiện khi đối tượng SpeechRecognition kết thúc
-    recognition.onend = () => {
-        if (isRecording) {
-            recognition.start(); // Bắt đầu lại nhận diện sau khi kết thúc
-        }
-    };
+  // Xử lý sự kiện khi đối tượng SpeechRecognition kết thúc
+  recognition.onend = () => {
+    if (isRecording) {
+      recognition.start(); // Bắt đầu lại nhận diện sau khi kết thúc
+    }
+  };
 
-    // Tự động bắt đầu nhận diện khi trang được tải
-    window.onload = () => {
-        recognition.start();
-    };
+  // Tự động bắt đầu nhận diện khi trang được tải
+  window.onload = () => {
+    recognition.start();
+  };
 } else {
-    alert('Trình duyệt không hỗ trợ chức năng này.');
+  alert("Trình duyệt không hỗ trợ chức năng này.");
 }
